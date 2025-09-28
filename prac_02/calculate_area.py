ecac4f29296ec137966b07dedcae604ef46aa37f
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-09-28 21:07:03 +0800

    initial program structure with main() and function calls

---
+++ prac_02/calculate_area.py
@@ -0,0 +1,15 @@
+# CONSTANTS
+EXAMPLE_CONSTANT = 10
+
+
+def main():
+    """ Main function that controls the overall program flow"""
+    do_stuff()
+    show_result()
+
+
+def do_stuff():
+
+def show_result():
+
+main()
