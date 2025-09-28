a4c0e4b7f1c2a3ad64d94deab8e718dda54e9d39
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-09-28 21:32:49 +0800

    Add display_menu() function for menu options

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -4,9 +4,7 @@ Program to get a valid score, print its result, and show stars equal to the scor
 """
 
 def main():
-    """
-    Display a menu to get a valid score, show result, or display stars.
-    """
+    """Display a menu to get a valid score, show result, or display stars"""
     score = get_valid_score()
     choice = display_menu()
     while choice != "Q":
@@ -22,7 +20,12 @@ def main():
     print("Farewell! Thank you for using the program.")
 
 def display_menu():
-
+    """Show the menu and return the user's uppercase choice"""
+    print("(G)et a valid score")
+    print("(P)rint result")
+    print("(S)how stars")
+    print("(Q)uit")
+    return input(">>> ").upper()
 
 def get_valid_score():
 
