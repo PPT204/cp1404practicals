1cf3175740b4239e3e0c77806830969034677f29
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-10-05 19:31:15 +0800

    Add uniform() example for float range and random number generator between 1 and 100

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -14,3 +14,9 @@ print(random.randrange(3, 10, 2))
 # Smallest: 3, largest: 9
 # Could line 2 have produced a 4? -> No, because step=2 skips even numbers.
 
+# Produces a float between 2.5 and 5.5
+print(random.uniform(2.5, 5.5))
+# Smallest possible: 2.5, largest possible: 5.5
+
+# Random integer between 1 and 100
+print(random.randint(1, 100))
