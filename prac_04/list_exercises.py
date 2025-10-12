847debbc6b7685b61108d70f8f52f6031243fa57
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-10-12 19:28:44 +0800

    Add function to display list statistics like: first, last, min, max, average

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,7 +6,7 @@ Basic list operations and simple security checker.
 def main():
     """Run the main program: number operations and security check."""
     numbers = get_numbers(5)
-
+    display_number_info(numbers)
 
 
 def get_numbers(count):
@@ -17,6 +17,18 @@ def get_numbers(count):
         numbers.append(number)
     return numbers
 
+def display_number_info(numbers):
+    """Display first, last, smallest, largest, and average of the list of numbers."""
+    first_number = numbers[0]
+    last_number = numbers[-1]
+    smallest_number = min(numbers)
+    largest_number = max(numbers)
+    average_number = sum(numbers) / len(numbers)
 
+    print(f"The first number is {first_number}")
+    print(f"The last number is {last_number}")
+    print(f"The smallest number is {smallest_number}")
+    print(f"The largest number is {largest_number}")
+    print(f"The average of the numbers is {average_number}")
 
 main()
