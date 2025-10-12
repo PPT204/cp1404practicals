d51c0219392fba2287f43bfbe8f4ef780b20a38c
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-10-12 19:30:26 +0800

    Implement simple security checker with username list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -7,7 +7,7 @@ def main():
     """Run the main program: number operations and security check."""
     numbers = get_numbers(5)
     display_number_info(numbers)
-
+    check_user_access()
 
 def get_numbers(count):
     """Prompt the user to enter 'count' numbers and return them as a list of floats."""
@@ -30,5 +30,17 @@ def display_number_info(numbers):
     print(f"The smallest number is {smallest_number}")
     print(f"The largest number is {largest_number}")
     print(f"The average of the numbers is {average_number}")
+
+def check_user_access():
+    """Check if entered username is in the list of authorised usernames."""
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+                 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+                 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    username = input("Enter your username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 main()
