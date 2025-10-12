070036500c563c73c0fb985c8c99825f614dbcad
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-10-12 19:41:33 +0800

    Add main function to handle user input

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,3 +8,22 @@ import random
 MIN_NUMBER = 1
 MAX_NUMBER = 45
 NUMBERS_PER_LINE = 6
+
+def main():
+    """Ask for number of quick picks and generate each lottery line."""
+    number_of_picks = int(input("How many quick picks? "))
+    for _ in range(number_of_picks):
+        quick_pick = generate_quick_pick()
+        print_quick_pick(quick_pick)
+
+
+def generate_quick_pick():
+
+
+
+
+def print_quick_pick(quick_pick):
+
+
+
+main()
