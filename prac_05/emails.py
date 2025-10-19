75c19cc4f586c04a59ad964472485759004e7cb6
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-10-19 20:38:26 +0800

    Add emails.py to collect and display names from email addresses

---
+++ prac_05/emails.py
@@ -0,0 +1,15 @@
+"""
+Emails
+Estimate: 15 minutes
+Actual: 17 minutes
+"""
+
+def main():
+    """Store emails and associated names, then display them."""
+    email_to_name = {}
+    email = input("Email: ")
+
+
+
+
+main()
