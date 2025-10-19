74fa1e04d0c5e0b5d2e41b451e495f420ef4519c
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-10-19 20:57:16 +0800

    Add data processing for champions and countries

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,7 +9,7 @@ FILENAME = "wimbledon.csv"
 def main():
     """Read data from file and display champions and their countries."""
     data = read_wimbledon_data(FILENAME)
-
+    champion_to_wins, countries = process_wimbledon_data(data)
 
 def read_wimbledon_data(filename):
     """Read the Wimbledon CSV file and return data excluding the header."""
@@ -17,3 +17,14 @@ def read_wimbledon_data(filename):
         lines = in_file.readlines()[1:]  # skip header
     data = [line.strip().split(",") for line in lines]
     return data
+
+def process_wimbledon_data(data):
+    """Process data into a dictionary of champions to wins, and a set of countries."""
+    champion_to_wins = {}
+    countries = set()
+    for record in data:
+        champion = record[2]
+        country = record[1]
+        champion_to_wins[champion] = champion_to_wins.get(champion, 0) + 1
+        countries.add(country)
+    return champion_to_wins, countries
