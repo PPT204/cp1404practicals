b54269da78f01fc3fd2e430b46c3f616bc439b1d
Pyae Phyo Tun <ifrit1090@gmail.com>
2025-10-19 19:16:21 +0800

    Add hex_colours.py with dictionary of colour codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical
+Hexadecimal colour lookup
+"""
+
+# Constant dictionary of colour names and their hex codes
+COLOUR_CODES = {
+    "ALICEBLUE": "#F0F8FF",
+    "ANTIQUEWHITE": "#FAEBD7",
+    "AQUA": "#00FFFF",
+    "AQUAMARINE": "#7FFFD4",
+    "AZURE": "#F0FFFF",
+    "BEIGE": "#F5F5DC",
+    "BISQUE": "#FFE4C4",
+    "BLACK": "#000000",
+    "BLUE": "#0000FF",
+    "BROWN": "#A52A2A"
+}
+
