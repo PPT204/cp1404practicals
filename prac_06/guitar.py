91c9e89fa2347dba554c30740e4d31d30c84919c
PhyoTun04 <ifrit1090@gmail.com>
2025-11-02 02:31:03 +0800

    Add __str__ method for formatted, is_vintage and  get_age method for Guitar display

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,6 +4,8 @@ Estimate: 10 minutes
 Actual:    minutes
 """
 
+CURRENT_YEAR = 2022
+VINTAGE_AGE = 50
 
 class Guitar:
     """Represent a Guitar object."""
@@ -13,3 +15,16 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a human-readable string representation of the Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the age of the guitar in years."""
+        CURRENT_YEAR = 2022
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old."""
+        return self.get_age() >= 50
