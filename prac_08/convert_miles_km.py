d05a94eb219627cee3b756dbe6ad188cf00eec84
PhyoTun04 <ifrit1090@gmail.com>
2025-11-21 16:56:46 +0800

    Implement miles-to-km conversion logic

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -11,5 +11,14 @@ class MilesKmApp(App):
         self.title = "Convert Miles to Kilometres"
         return Builder.load_file("convert_miles_km.kv")
 
+    def handle_convert(self):
+        try:
+            miles = float(self.root.ids.input_miles.text)
+        except ValueError:
+            miles = 0
+
+        km = miles * MILES_TO_KM
+        self.km_output = str(round(km, 5))
+        
 if __name__ == "__main__":
     MilesKmApp().run()
