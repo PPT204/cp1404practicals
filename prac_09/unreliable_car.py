16e2b8f91eff1dc5ca50cfeaabfcdac338d80332
PhyoTun04 <ifrit1090@gmail.com>
2025-11-22 00:54:29 +0800

    Add unreliable_car_test.py to demonstrate UnreliableCar behaviour, update UnreliableCar.__init__ so super().__init__ receives only fuel

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,16 +1,23 @@
+from prac_09.car import Car
 import random
-from car import Car
 
 
 class UnreliableCar(Car):
-    """Specialised Car that may fail to drive."""
+    """Car that sometimes fails to drive based on reliability."""
 
-    def __init__(self, name: str, fuel: float, reliability: float):
-        super().__init__(name, fuel)
+    def __init__(self, name, fuel, reliability):
+        super().__init__(fuel)
+        self.name = name
         self.reliability = reliability
 
-    def drive(self, distance: float) -> float:
-        random_chance = random.uniform(0, 100)
-        if random_chance < self.reliability:
+    def __str__(self):
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}, reliability={self.reliability}%"
+
+    def drive(self, distance):
+        """Drive only if random check passes."""
+        if random.uniform(0, 100) < self.reliability:
+            # drive normally
             return super().drive(distance)
-        return 0
+        else:
+            # unreliable car fails to move
+            return 0
